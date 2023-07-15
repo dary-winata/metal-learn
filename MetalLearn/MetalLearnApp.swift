2f151dfa67199c6c0385adf1f5b49f3f27bf6604
dary-winata <dary.winata15@gmail.com>
2023-07-15 20:04:38 +0700

    Initial Commit

---
+++ MetalLearn/MetalLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalLearnApp.swift
+//  MetalLearn
+//
+//  Created by dary winata nugraha djati on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MetalLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
